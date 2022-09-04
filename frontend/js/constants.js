758c2c49e8db41f55853cadd79eb0ca9c513175a
natedogdonedidit <natedodds09@gmail.com>
2022-09-04 18:18:35 -0400

    deleted:    backend/.env.example 	modified:   backend/src/config.js 	modified:   frontend/js/constants.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0xb0dC649AD9a1D3922BF675C2Ba9fe0a7DB7ccf86"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "It's time for the 🧊 🐒's to EAT.";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Business Cards are the 1st of 3 collections by the Cube Monkeys. Join us as we build the future of work. Business Card owners will have access to our rewards program and Hallway beta. Learn more on our website ya stupid Cube Monkey!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
